3c7d6153f898510df169fec34c052a88b57cd849
Olanrewaju <114247644+Olanrewaju-ola@users.noreply.github.com>
2022-10-30 12:27:57 -0700

    Create Dbh.php

---
+++ UserAuthOOPMysql/classes/Dbh.php
@@ -0,0 +1,32 @@
+<?php
+
+// Creating Class Dbh
+ class Dbh{
+
+        private $hostname;
+        private $username;
+        private $password;
+        private $dbname;
+
+    // Creating the class properties
+
+    protected function connect(){
+       $this->hostname = 'localhost';
+       $this->username = 'olanrewaju';
+       $this->password = 'olanrewaju';
+       $this->dbname = 'zuriphp';
+
+       $conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->dbname);
+
+       if(!$conn){
+        echo "<script> alert('Error connecting to the database') </script>";
+    }
+    return $conn;
+    }
+}
+    
+
+
+    
+
+
